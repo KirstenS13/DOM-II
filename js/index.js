0fc7824e682b07ae73e6691753e0f367cc572f76
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-04-12 19:50:46 -0400

    prevented default on nav links, added first event on the buttons at bottom of page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 // Your code goes here
+
+//selecting nav links
+const navLinks = document.querySelectorAll('.nav-link');
+//preventing default behaviour of nav links
+navLinks.forEach((navLinkEl) => {
+    navLinkEl.addEventListener('click', (event) => {
+        event.preventDefault();
+    })
+})
+
+//Event 1..................
+//Select buttons
+const buttons = document.querySelectorAll('.btn');
+//enlarge buttons when mouse enters
+buttons.forEach((btnEl) => {
+    btnEl.addEventListener('mouseenter', (event) => {
+        event.target.style.transform = 'scale(1.1)';
+        event.target.style.transition = 'transform 1s';
+    })
+})
+//reduce buttons when mouse leaves
+buttons.forEach((btnEl) => {
+    btnEl.addEventListener('mouseleave', (event) => {
+        event.target.style.transform = 'scale(1)';
+    })
+})
+
+//Event 2..................
